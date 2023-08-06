5c563929ecf328a75366cffd3ca567fa470ffc6c
Mateusz Mazur <mateusz.mazur@e.email>
2023-08-06 18:56:58 +0200

    Add test infra (#13)
    * Add test infra
    
    * Add unit tests to CI
    
    * Fix python incompatibility
    
    * Dont ignore test result

---
+++ src/logs/test_types.py
@@ -0,0 +1,38 @@
+from io import StringIO
+
+from src.logs.types import LogFile, LogLine
+
+import pytest
+
+def test_logfile():
+    sample_file = StringIO(
+    '''
+    Line1
+    Line2
+    Line3
+    '''.strip())
+
+    log_file = LogFile(sample_file)
+    assert len(log_file.lines) == 3, "There are 3 lines"
+
+    first_line = log_file.lines[0]
+    assert first_line.line == "Line1\n"
+
+def test_logline_heuristic():
+    line = LogLine("test")
+
+    assert line.line == "test", "String is saved properly"
+
+    line.add_heuristic("Sample", 0.5)
+    assert line.get_heuristic("Sample") == 0.5, "It is possible to add heuristic"
+
+    line.clear_heuristics()
+
+    # There should be no heurisric now
+    with pytest.raises(ValueError):
+        line.get_heuristic("Sample")
+
+    # There should be no template initially
+    with pytest.raises(ValueError):
+        line.template
+
