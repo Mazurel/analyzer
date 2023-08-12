7c28c19ba67291c25734c0fed23dd8b9cf55bf90
Mateusz Mazur <mateusz.mazur@e.email>
2023-08-12 12:39:44 +0200

    Implement automatic regex finder (#14)
    Signed-off-by: Mateusz Mazur <mateusz.mazur@e.email>

--- src/heuristics/manager.py
+++ src/heuristics/manager.py
@@ -1,15 +1,13 @@
 from src.logs.types import LogFile
 from src.heuristics.types import Heuristic
-from src.heuristics.random import RandomHeuristic
 from src.heuristics.histogram import HistogramHeuristic
 
-HEURISTICS: list[tuple[str, Heuristic]] = [("Histogram", HistogramHeuristic)]
+HEURISTICS: list[tuple[str, Heuristic]] = [("Histogram", HistogramHeuristic())]
 
 
 def apply_heuristics(grand_truth: LogFile, checked: LogFile):
     grand_truth.clear_heuristics()
     checked.clear_heuristics()
-    for name, heuristic_type in HEURISTICS:
-        heuristic = heuristic_type()
+    for name, heuristic in HEURISTICS:
         heuristic.load_grand_truth(grand_truth)
         heuristic.calculate_heuristic(name, checked)
