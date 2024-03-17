490185f994763f4afa36ee766fe2cfa8180ff551
Mateusz Mazur <mateusz.mazur@e.email>
2024-03-17 21:27:08 +0100

    Implement simple TUI interactive app
    Signed-off-by: Mateusz Mazur <mateusz.mazur@e.email>

---
+++ analyzer/console-app/src/main/kotlin/org/analyzer/console/App.kt
@@ -0,0 +1,54 @@
+package org.analyzer.kotlin.console
+
+import org.analyzer.kotlin.log.parsers.DictParser
+
+import kotlinx.coroutines.delay
+
+import com.varabyte.kotter.foundation.text.*
+import com.varabyte.kotter.foundation.input.*
+import com.varabyte.kotter.foundation.*
+
+fun main() = session {
+    var inputLine by liveVarOf<String?>(null)
+    val parser = DictParser()
+
+    section {
+        if (inputLine == null) {
+            text("Provide a line to be parsed: ")
+            input()
+        }
+        else {
+            val pattern = parser.extractPatternFromLine(inputLine!!)
+            val id = parser.putPattern(pattern)
+            pattern.humanReadable().let {
+                text("Input line: $inputLine\n")
+                text("Pattern: $it\n")
+                text("Pattern ID: $id\n")
+                text("Press enter to continue ...")
+            }
+        }
+    }.run {
+        onInputEntered {
+            when (inputLine) {
+                null -> {
+                  inputLine = input
+                }
+            }
+        }
+
+        onKeyPressed {
+            when (key) {
+                Keys.ENTER -> {
+                  if (inputLine != null) {
+                    inputLine = null
+                  }
+                }
+            }
+        }
+
+        while (true) {
+            delay(100)
+        }
+    }
+}
+
